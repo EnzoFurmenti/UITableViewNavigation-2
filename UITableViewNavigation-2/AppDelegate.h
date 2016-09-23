621576a645f3e90390969e2ba36981a4aadf1aa7
EnzoFurmenti <ducheenzo@mail.ru>
2016-09-23 14:14:10 +0300

    Initial Commit

---
+++ UITableViewNavigation-2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewNavigation-2
+//
+//  Created by EnzoF on 23.09.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewNavigation-2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewNavigation-2
+//
+//  Created by EnzoF on 23.09.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
